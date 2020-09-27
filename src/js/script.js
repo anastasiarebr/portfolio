628db254daf63b5187e640b0b8d48db98676412b
Anastasia <anastasiarebr@gmail.com>
2020-09-27 18:13:07 +0300

    portfolio price skills

--- src/js/script.js
+++ src/js/script.js
@@ -10,3 +10,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const percents = document.querySelectorAll('.skills__percent-title span'),
+      lines = document.querySelectorAll('.skills__percent-line-change');
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+}); 
